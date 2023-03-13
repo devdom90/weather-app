5b2ce298586b049715f245300637a847ee3893ce
devdom90 <devdom@protonmail.com>
2023-03-13 13:23:21 +0100

    changing config terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,38 +1,61 @@
 terraform {
   required_providers {
     azurerm = {
-        source  = "hashicorp/azurerm"
-        version = "=3.0.0"
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
     }
   }
 }
 
+# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
 
-
-resource "azurerm_resource_group" "test-rg" {
-  name = "weather-app-rg"
+# Create a resource group
+resource "azurerm_resource_group" "test_rg" {
+  name     = "test-rg"
   location = "West Europe"
 }
 
-resource "azurerm_container_group" "test-acg" {
-  name                = "w-app-acg-1"
-  location            = azurerm_resource_group.test-rg.location
-  resource_group_name = azurerm_resource_group.test-rg.name
-  ip_address_type     = "Public"
-  dns_name_label      = "weather-app"
-  os_type             = "Linux"
-  container {
-    name              = "weatherapp"
-    image             = "devdom90/w-app-acg-1"
-      cpu               = "1"
-      memory            = "1"
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "test_vn" {
+  name                = "test-network"
+  resource_group_name = azurerm_resource_group.test_rg.name
+  location            = azurerm_resource_group.test_rg.location
+  address_space       = ["10.0.0.0/16"]
+}
 
-      ports {
-        port            = 80
-        protocol        = "TCP"  
-      } 
+resource "azurerm_kubernetes_cluster" "test_aks" {
+  name                = "test-aks1"
+  location            = azurerm_resource_group.test_rg.location
+  resource_group_name = azurerm_resource_group.test_rg.name
+  dns_prefix          = "testingaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
   }
 }
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.test_aks.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.test_aks.kube_config_raw
+
+  sensitive = true
+}
+
+
