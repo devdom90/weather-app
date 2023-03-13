e79425324b5c094976c6b5bce8d26e11286a3eb6
devdom90 <devdom@protonmail.com>
2023-03-13 13:31:43 +0100

    change everything

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,44 +18,26 @@ resource "azurerm_resource_group" "test_rg" {
   location = "West Europe"
 }
 
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "test_vn" {
-  name                = "test-network"
+resource "azurerm_container_group" "test_cg" {
+  name                = "test-container-group"
   resource_group_name = azurerm_resource_group.test_rg.name
   location            = azurerm_resource_group.test_rg.location
-  address_space       = ["10.0.0.0/16"]
-}
+  ip_address_type     = "Public"
+  dns_name_label      = "aci-label"
+  os_type             = "Linux"
 
-resource "azurerm_kubernetes_cluster" "test_aks" {
-  name                = "test-aks1"
-  location            = azurerm_resource_group.test_rg.location
-  resource_group_name = azurerm_resource_group.test_rg.name
-  dns_prefix          = "testingaks1"
+container {
+  name   = "weather-app"
+  image  = "mcr.microsoft.com/azuredocs/aci-helloworld:latest"
+  cpu    = "1"
+  memory = "1"
 
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_B2s"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = {
-    Environment = "Production"
+  ports {
+    port     = 80
+    protocol = "TCP"
   }
 }
 
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.test_aks.kube_config.0.client_certificate
-  sensitive = true
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.test_aks.kube_config_raw
-
-  sensitive = true
 }
 
 
